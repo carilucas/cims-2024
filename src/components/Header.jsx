8bc580361cca4a86b311c1561847e0918f4d8be9
Carlos Mora <carlos.mora.ram@gmail.com>
2024-08-15 14:05:54 -0500

    correo de contacto agregado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
                     </div>
                     <div className="menu">
                         <div className="flex gap-1 text-slate-600 items-center">
-                            <a href="mailto:">Contacto </a>
+                            <a href="mailto:igduenase@puce.edu.ec">Contacto </a>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
                             </svg>
